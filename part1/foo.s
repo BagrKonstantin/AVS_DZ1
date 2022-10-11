315f4fb8fc84824e1327466e75341e3edb02635e
BagrKonstantin <57901534+BagrKonstantin@users.noreply.github.com>
2022-10-11 21:29:55 +0300

    Update foo.s

--- part1/foo.s
+++ part1/foo.s
@@ -1,5 +1,4 @@
-	.file	"cod.c"
-	.intel_syntax noprefix
+	.file	"code.c"
 	.text
 	.local	a
 	.comm	a,262144,32
@@ -14,114 +13,130 @@
 	.globl	main
 	.type	main, @function
 main:
+.LFB0:
+	.cfi_startproc
 	endbr64
-	push	rbp
-	mov	rbp, rsp
-	sub	rsp, 16
-	mov	DWORD PTR -8[rbp], 0
-	mov	DWORD PTR -12[rbp], 0
-	lea	rax, -16[rbp]
-	mov	rsi, rax
-	lea	rax, .LC0[rip]
-	mov	rdi, rax
-	mov	eax, 0
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$0, -16(%rbp)
+	movl	$0, -12(%rbp)
+	leaq	-24(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
 	call	__isoc99_scanf@PLT
-	mov	DWORD PTR -4[rbp], 0
+	movl	$0, -20(%rbp)
 	jmp	.L2
 .L3:
-	mov	eax, DWORD PTR -4[rbp]
-	cdqe
-	lea	rdx, 0[0+rax*4]
-	lea	rax, a[rip]
-	add	rax, rdx
-	mov	rsi, rax
-	lea	rax, .LC0[rip]
-	mov	rdi, rax
-	mov	eax, 0
+	movl	-20(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	leaq	a(%rip), %rax
+	addq	%rdx, %rax
+	movq	%rax, %rsi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
 	call	__isoc99_scanf@PLT
-	add	DWORD PTR -4[rbp], 1
+	addl	$1, -20(%rbp)
 .L2:
-	mov	eax, DWORD PTR -16[rbp]
-	cmp	DWORD PTR -4[rbp], eax
+	movl	-24(%rbp), %eax
+	cmpl	%eax, -20(%rbp)
 	jl	.L3
-	mov	DWORD PTR -4[rbp], 0
+	movl	$0, -20(%rbp)
 	jmp	.L4
 .L7:
-	mov	eax, DWORD PTR -4[rbp]
-	cdqe
-	lea	rdx, 0[0+rax*4]
-	lea	rax, a[rip]
-	mov	eax, DWORD PTR [rdx+rax]
-	test	eax, eax
+	movl	-20(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	leaq	a(%rip), %rax
+	movl	(%rdx,%rax), %eax
+	testl	%eax, %eax
 	jns	.L5
-	mov	eax, DWORD PTR -4[rbp]
-	cdqe
-	lea	rdx, 0[0+rax*4]
-	lea	rax, a[rip]
-	mov	eax, DWORD PTR [rdx+rax]
-	add	DWORD PTR -8[rbp], eax
+	movl	-20(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	leaq	a(%rip), %rax
+	movl	(%rdx,%rax), %eax
+	addl	%eax, -16(%rbp)
 	jmp	.L6
 .L5:
-	mov	eax, DWORD PTR -4[rbp]
-	cdqe
-	lea	rdx, 0[0+rax*4]
-	lea	rax, a[rip]
-	mov	eax, DWORD PTR [rdx+rax]
-	add	DWORD PTR -12[rbp], eax
+	movl	-20(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	leaq	a(%rip), %rax
+	movl	(%rdx,%rax), %eax
+	addl	%eax, -12(%rbp)
 .L6:
-	add	DWORD PTR -4[rbp], 1
+	addl	$1, -20(%rbp)
 .L4:
-	mov	eax, DWORD PTR -16[rbp]
-	cmp	DWORD PTR -4[rbp], eax
+	movl	-24(%rbp), %eax
+	cmpl	%eax, -20(%rbp)
 	jl	.L7
-	mov	DWORD PTR -4[rbp], 0
+	movl	$0, -20(%rbp)
 	jmp	.L8
 .L11:
-	mov	eax, DWORD PTR -4[rbp]
-	and	eax, 1
-	test	eax, eax
+	movl	-20(%rbp), %eax
+	andl	$1, %eax
+	testl	%eax, %eax
 	jne	.L9
-	mov	eax, DWORD PTR -4[rbp]
-	cdqe
-	lea	rcx, 0[0+rax*4]
-	lea	rdx, b[rip]
-	mov	eax, DWORD PTR -12[rbp]
-	mov	DWORD PTR [rcx+rdx], eax
+	movl	-20(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rcx
+	leaq	b(%rip), %rdx
+	movl	-12(%rbp), %eax
+	movl	%eax, (%rcx,%rdx)
 	jmp	.L10
 .L9:
-	mov	eax, DWORD PTR -4[rbp]
-	cdqe
-	lea	rcx, 0[0+rax*4]
-	lea	rdx, b[rip]
-	mov	eax, DWORD PTR -8[rbp]
-	mov	DWORD PTR [rcx+rdx], eax
+	movl	-20(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rcx
+	leaq	b(%rip), %rdx
+	movl	-16(%rbp), %eax
+	movl	%eax, (%rcx,%rdx)
 .L10:
-	add	DWORD PTR -4[rbp], 1
+	addl	$1, -20(%rbp)
 .L8:
-	mov	eax, DWORD PTR -16[rbp]
-	cmp	DWORD PTR -4[rbp], eax
+	movl	-24(%rbp), %eax
+	cmpl	%eax, -20(%rbp)
 	jl	.L11
-	mov	DWORD PTR -4[rbp], 0
+	movl	$0, -20(%rbp)
 	jmp	.L12
 .L13:
-	mov	eax, DWORD PTR -4[rbp]
-	cdqe
-	lea	rdx, 0[0+rax*4]
-	lea	rax, b[rip]
-	mov	eax, DWORD PTR [rdx+rax]
-	mov	esi, eax
-	lea	rax, .LC1[rip]
-	mov	rdi, rax
-	mov	eax, 0
+	movl	-20(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	leaq	b(%rip), %rax
+	movl	(%rdx,%rax), %eax
+	movl	%eax, %esi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
 	call	printf@PLT
-	add	DWORD PTR -4[rbp], 1
+	addl	$1, -20(%rbp)
 .L12:
-	mov	eax, DWORD PTR -16[rbp]
-	cmp	DWORD PTR -4[rbp], eax
+	movl	-24(%rbp), %eax
+	cmpl	%eax, -20(%rbp)
 	jl	.L13
-	mov	eax, 0
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L15
+	call	__stack_chk_fail@PLT
+.L15:
 	leave
+	.cfi_def_cfa 7, 8
 	ret
+	.cfi_endproc
+.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 11.2.0-19ubuntu1) 11.2.0"
 	.section	.note.GNU-stack,"",@progbits
