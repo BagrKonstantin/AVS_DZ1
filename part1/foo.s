35ca8818606c5497e371e0191bb7a9130b048d42
BagrKonstantin <57901534+BagrKonstantin@users.noreply.github.com>
2022-10-11 21:01:45 +0300

    Update foo.s

--- part1/foo.s
+++ part1/foo.s
@@ -5,19 +5,18 @@
 main:
         push    rbp
         mov     rbp, rsp
-        sub     rsp, 32
-        mov     DWORD PTR [rbp-28], 0
-        mov     DWORD PTR [rbp-4], 0
+        sub     rsp, 16
         mov     DWORD PTR [rbp-8], 0
-        lea     rax, [rbp-28]
+        mov     DWORD PTR [rbp-12], 0
+        lea     rax, [rbp-16]
         mov     rsi, rax
         mov     edi, OFFSET FLAT:.LC0
         mov     eax, 0
         call    __isoc99_scanf
-        mov     DWORD PTR [rbp-12], 0
+        mov     DWORD PTR [rbp-4], 0
         jmp     .L2
 .L3:
-        mov     eax, DWORD PTR [rbp-12]
+        mov     eax, DWORD PTR [rbp-4]
         cdqe
         sal     rax, 2
         add     rax, OFFSET FLAT:a
@@ -25,72 +24,72 @@ main:
         mov     edi, OFFSET FLAT:.LC0
         mov     eax, 0
         call    __isoc99_scanf
-        add     DWORD PTR [rbp-12], 1
+        add     DWORD PTR [rbp-4], 1
 .L2:
-        mov     eax, DWORD PTR [rbp-28]
-        cmp     DWORD PTR [rbp-12], eax
+        mov     eax, DWORD PTR [rbp-16]
+        cmp     DWORD PTR [rbp-4], eax
         jl      .L3
-        mov     DWORD PTR [rbp-16], 0
+        mov     DWORD PTR [rbp-4], 0
         jmp     .L4
 .L7:
-        mov     eax, DWORD PTR [rbp-16]
+        mov     eax, DWORD PTR [rbp-4]
         cdqe
         mov     eax, DWORD PTR a[0+rax*4]
         test    eax, eax
         jns     .L5
-        mov     eax, DWORD PTR [rbp-16]
-        cdqe
-        mov     eax, DWORD PTR a[0+rax*4]
-        add     DWORD PTR [rbp-4], eax
-        jmp     .L6
-.L5:
-        mov     eax, DWORD PTR [rbp-16]
+        mov     eax, DWORD PTR [rbp-4]
         cdqe
         mov     eax, DWORD PTR a[0+rax*4]
         add     DWORD PTR [rbp-8], eax
+        jmp     .L6
+.L5:
+        mov     eax, DWORD PTR [rbp-4]
+        cdqe
+        mov     eax, DWORD PTR a[0+rax*4]
+        add     DWORD PTR [rbp-12], eax
 .L6:
-        add     DWORD PTR [rbp-16], 1
+        add     DWORD PTR [rbp-4], 1
 .L4:
-        mov     eax, DWORD PTR [rbp-28]
-        cmp     DWORD PTR [rbp-16], eax
+        mov     eax, DWORD PTR [rbp-16]
+        cmp     DWORD PTR [rbp-4], eax
         jl      .L7
-        mov     DWORD PTR [rbp-20], 0
+        mov     DWORD PTR [rbp-4], 0
         jmp     .L8
 .L11:
-        mov     eax, DWORD PTR [rbp-20]
+        mov     eax, DWORD PTR [rbp-4]
         and     eax, 1
         test    eax, eax
         jne     .L9
-        mov     eax, DWORD PTR [rbp-20]
+        mov     eax, DWORD PTR [rbp-4]
         cdqe
-        mov     edx, DWORD PTR [rbp-8]
+        mov     edx, DWORD PTR [rbp-12]
         mov     DWORD PTR b[0+rax*4], edx
         jmp     .L10
 .L9:
-        mov     eax, DWORD PTR [rbp-20]
+        mov     eax, DWORD PTR [rbp-4]
         cdqe
-        mov     edx, DWORD PTR [rbp-4]
+        mov     edx, DWORD PTR [rbp-8]
         mov     DWORD PTR b[0+rax*4], edx
 .L10:
-        add     DWORD PTR [rbp-20], 1
+        add     DWORD PTR [rbp-4], 1
 .L8:
-        mov     eax, DWORD PTR [rbp-28]
-        cmp     DWORD PTR [rbp-20], eax
+        mov     eax, DWORD PTR [rbp-16]
+        cmp     DWORD PTR [rbp-4], eax
         jl      .L11
-        mov     DWORD PTR [rbp-24], 0
+        mov     DWORD PTR [rbp-4], 0
         jmp     .L12
 .L13:
-        mov     eax, DWORD PTR [rbp-24]
+        mov     eax, DWORD PTR [rbp-4]
         cdqe
         mov     eax, DWORD PTR b[0+rax*4]
         mov     esi, eax
         mov     edi, OFFSET FLAT:.LC1
         mov     eax, 0
         call    printf
-        add     DWORD PTR [rbp-24], 1
+        add     DWORD PTR [rbp-4], 1
 .L12:
-        mov     eax, DWORD PTR [rbp-28]
-        cmp     DWORD PTR [rbp-24], eax
+        mov     eax, DWORD PTR [rbp-16]
+        cmp     DWORD PTR [rbp-4], eax
         jl      .L13
         mov     eax, 0
         leave
