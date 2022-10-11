9de3bb2a7fbabb57746a65e578858380af88375a
BagrKonstantin <57901534+BagrKonstantin@users.noreply.github.com>
2022-10-11 21:00:55 +0300

    Update foo.c

--- part1/foo.c
+++ part1/foo.c
@@ -5,22 +5,22 @@ static int b[65536];
 
 
 int main() {
-    int n = 0;
+    int n, i;
     int sum_m = 0;
     int sum_p = 0;
 
     scanf("%d", &n);
-    for (int i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
         scanf("%d", &a[i]);
     }
-    for (int i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
         if (a[i] < 0) {
             sum_m += a[i];
         } else {
             sum_p += a[i];
         }
     }
-    for (int i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
         if (i % 2 == 0) {
             b[i] = sum_p;
         } else {
@@ -28,7 +28,7 @@ int main() {
         }
     }
 
-    for (int i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
         printf("%d ", b[i]);
     }
     return 0;
