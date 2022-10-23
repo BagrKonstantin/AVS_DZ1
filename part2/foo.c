40654f65a8bb2699641d1217438455bac1bf6b36
BagrKonstantin <57901534+BagrKonstantin@users.noreply.github.com>
2022-10-23 17:44:21 +0300

    Update foo.c

--- part2/foo.c
+++ part2/foo.c
@@ -3,7 +3,6 @@
 static int a[65536];
 static int b[65536];
 
-
 void fill_array(int sum_m, int sum_p, int n) {
     for (int i = 0; i < n; ++i) {
         if (i % 2 == 0) {
@@ -14,23 +13,37 @@ void fill_array(int sum_m, int sum_p, int n) {
     }
 }
 
+int find_neg_sum(int n) {
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        if (a[i] < 0) {
+            sum += a[i];
+        }
+    }
+    return sum;
+}
+
+int find_pos_sum(int n) {
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        if (a[i] > 0) {
+            sum += a[i];
+        }
+    }
+    return sum;
+}
 
 int main() {
     int n, i;
-    int sum_m = 0;
-    int sum_p = 0;
+    int sum_m;
+    int sum_p;
 
     scanf("%d", &n);
     for (i = 0; i < n; ++i) {
         scanf("%d", &a[i]);
     }
-    for (i = 0; i < n; ++i) {
-        if (a[i] < 0) {
-            sum_m += a[i];
-        } else {
-            sum_p += a[i];
-        }
-    }
+    sum_m = find_neg_sum(n);
+    sum_p = find_pos_sum(n);
     fill_array(sum_m, sum_p, n);
     for (i = 0; i < n; ++i) {
         printf("%d ", b[i]);
