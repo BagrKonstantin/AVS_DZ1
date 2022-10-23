5dfe8a7ab88db433087697a5f9b55552ec51fb8d
BagrKonstantin <57901534+BagrKonstantin@users.noreply.github.com>
2022-10-23 22:18:42 +0300

    Create old_foo.s

---
+++ part3/old_foo.s
@@ -0,0 +1,187 @@
+.file	"foo.c"
+	.intel_syntax noprefix
+	.text
+	.local	a
+	.comm	a,262144,32
+	.local	b
+	.comm	b,262144,32
+	.globl	find_neg_sum
+	.type	find_neg_sum, @function
+find_neg_sum:
+	endbr64
+	push	rbp
+	mov	rbp, rsp
+	mov	DWORD PTR -20[rbp], edi         
+	mov	DWORD PTR -4[rbp], 0           
+	mov	DWORD PTR -8[rbp], 0           
+	jmp	.L2
+.L4:
+	mov	eax, DWORD PTR -8[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*4]
+	lea	rax, a[rip]
+	mov	eax, DWORD PTR [rdx+rax]
+	test	eax, eax
+	jns	.L3
+	mov	eax, DWORD PTR -8[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*4]
+	lea	rax, a[rip]
+	mov	eax, DWORD PTR [rdx+rax]
+	add	DWORD PTR -4[rbp], eax
+.L3:
+	add	DWORD PTR -8[rbp], 1
+.L2:
+	mov	eax, DWORD PTR -8[rbp]                 
+	cmp	eax, DWORD PTR -20[rbp]                
+	jl	.L4
+	mov	eax, DWORD PTR -4[rbp]                
+	pop	rbp
+	ret
+	.size	find_neg_sum, .-find_neg_sum
+	.globl	find_pos_sum
+	.type	find_pos_sum, @function
+find_pos_sum:
+	endbr64
+	push	rbp
+	mov	rbp, rsp
+	mov	DWORD PTR -20[rbp], edi               
+	mov	DWORD PTR -4[rbp], 0                    
+	mov	DWORD PTR -8[rbp], 0                  
+	jmp	.L7
+.L9:
+	mov	eax, DWORD PTR -8[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*4]
+	lea	rax, a[rip]
+	mov	eax, DWORD PTR [rdx+rax]
+	test	eax, eax
+	jle	.L8
+	mov	eax, DWORD PTR -8[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*4]
+	lea	rax, a[rip]
+	mov	eax, DWORD PTR [rdx+rax]
+	add	DWORD PTR -4[rbp], eax
+.L8:
+	add	DWORD PTR -8[rbp], 1
+.L7:
+	mov	eax, DWORD PTR -8[rbp]                
+	cmp	eax, DWORD PTR -20[rbp]
+	jl	.L9
+	mov	eax, DWORD PTR -4[rbp]                
+	pop	rbp
+	ret
+	.size	find_pos_sum, .-find_pos_sum
+	.section	.rodata
+.LC0:
+	.string	"%d"
+.LC1:
+	.string	"%d "
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	endbr64
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 16
+	lea	rax, -16[rbp]
+	mov	rsi, rax
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	__isoc99_scanf@PLT
+	mov	DWORD PTR -4[rbp], 0
+	jmp	.L12
+.L13:
+	mov	eax, DWORD PTR -4[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*4]
+	lea	rax, a[rip]
+	add	rax, rdx
+	mov	rsi, rax
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	__isoc99_scanf@PLT
+	add	DWORD PTR -4[rbp], 1
+.L12:
+	mov	eax, DWORD PTR -16[rbp]
+	cmp	DWORD PTR -4[rbp], eax
+	jl	.L13
+	mov	eax, DWORD PTR -16[rbp]
+	mov	edi, eax
+	call	find_neg_sum
+	mov	DWORD PTR -8[rbp], eax
+	mov	eax, DWORD PTR -16[rbp]
+	mov	edi, eax
+	call	find_pos_sum
+	mov	DWORD PTR -12[rbp], eax
+	mov	DWORD PTR -4[rbp], 0
+	jmp	.L14
+.L17:
+	mov	eax, DWORD PTR -4[rbp]
+	and	eax, 1
+	test	eax, eax
+	jne	.L15
+	mov	eax, DWORD PTR -4[rbp]
+	cdqe
+	lea	rcx, 0[0+rax*4]
+	lea	rdx, b[rip]
+	mov	eax, DWORD PTR -12[rbp]
+	mov	DWORD PTR [rcx+rdx], eax
+	jmp	.L16
+.L15:
+	mov	eax, DWORD PTR -4[rbp]
+	cdqe
+	lea	rcx, 0[0+rax*4]
+	lea	rdx, b[rip]
+	mov	eax, DWORD PTR -8[rbp]
+	mov	DWORD PTR [rcx+rdx], eax
+.L16:
+	add	DWORD PTR -4[rbp], 1
+.L14:
+	mov	eax, DWORD PTR -16[rbp]
+	cmp	DWORD PTR -4[rbp], eax
+	jl	.L17
+	mov	DWORD PTR -4[rbp], 0
+	jmp	.L18
+.L19:
+	mov	eax, DWORD PTR -4[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*4]
+	lea	rax, b[rip]
+	mov	eax, DWORD PTR [rdx+rax]
+	mov	esi, eax
+	lea	rax, .LC1[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	add	DWORD PTR -4[rbp], 1
+.L18:
+	mov	eax, DWORD PTR -16[rbp]
+	cmp	DWORD PTR -4[rbp], eax
+	jl	.L19
+	mov	eax, 0
+	leave
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.2.0-19ubuntu1) 11.2.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
